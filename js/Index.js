2a04b799ad286650d1ec5233d9e0b8595919540c
Theking Hubs <131630822+MarcosLima-1@users.noreply.github.com>
2023-06-25 07:13:44 -0300

    1.1

--- js/Index.js
+++ js/Index.js
@@ -56,10 +56,13 @@ projectstabs(0);
 skillstabs(0);
 
 function toggleDiv() {
-    var div = document.getElementById("background_ambuigue");
+    var div = document.getElementById("menu_bar_nav");
+    var header = document.getElementById("header_css");
     if (div.style.display === "flex") {
       div.style.display = "none";
+      header.style.borderRadius = "0 0 12px 12px"
     } else {
       div.style.display = "flex";
+      header.style.borderRadius = "0 0 0 0"
     }
   }
