f18d39602b3016d2b2d1546ae64e1f9535bf162d
Marcos <131630822+MarcosLima-1@users.noreply.github.com>
2024-04-17 18:34:13 -0300

    2.0 portifolio

--- js/Index.js
+++ js/Index.js
@@ -1,50 +1,24 @@
-var tabsbuttons = document.querySelectorAll(".section_4 .categorias .button")
-var tabsbuttons1 = document.querySelectorAll(".section_3 .categorias .button")
-var tabs = document.querySelectorAll(".tabs_1")
-var tabs1 = document.querySelectorAll(".tabs")
+var tabsbuttons = document.querySelectorAll('.section_4 .categorias .button');
+var tabs = document.querySelectorAll('.tabs_1');
 
-function projectstabs(panelindex){
-    tabsbuttons.forEach(function(node){
-        node.style.backgroundColor="#dedfe8";
-        node.style.color="#111218";
-    });
-
-    tabsbuttons[panelindex].style.backgroundColor="#323549";
-    tabsbuttons[panelindex].style.color="#dedfe8";
-
-    tabs.forEach(function(node){
-        node.style.display="none";
-    });
-    tabs[panelindex].style.display="grid";
-};
-
-function skillstabs(panelindex){
-
-    tabsbuttons1.forEach(function(node){
-        node.style.backgroundColor="#dedfe8";
-        node.style.color="#111218";
-    });
-
-
-    tabsbuttons1[panelindex].style.backgroundColor="#323549";
-    tabsbuttons1[panelindex].style.color="#dedfe8";
-
-    tabs1.forEach(function(node){
-        node.style.display="none";
-    });
-    tabs1[panelindex].style.display="flex";
-};
+function projectstabs(panelindex) {
+	tabsbuttons.forEach(function (node) {
+		node.classList.add('outline');
+	});
+	tabsbuttons[panelindex].classList.remove('outline');
+	tabs.forEach(function (node) {
+		node.style.display = 'none';
+	});
+	tabs[panelindex].style.display = 'grid';
+}
 projectstabs(0);
-skillstabs(0);
 
 function toggleDiv() {
-    var div = document.getElementById("menu_bar_nav");
-    var header = document.getElementById("header_css");
-    if (div.style.display === "flex") {
-      div.style.display = "none";
-      header.style.borderRadius = "0 0 12px 12px"
-    } else {
-      div.style.display = "flex";
-      header.style.borderRadius = "0 0 0 0"
-    }
-  }
+	var div = document.getElementById('menu_bar_nav');
+	var header = document.getElementById('header_css');
+	if (div.style.display === 'flex') {
+		div.style.display = 'none';
+	} else {
+		div.style.display = 'flex';
+	}
+}
